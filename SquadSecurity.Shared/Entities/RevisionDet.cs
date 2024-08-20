1b5fffc1cb2c93a7ebdd5eb979a76dca92d5c0ab
elara <inteco2207@gmail.com>
2024-08-20 15:49:49 -0500

    Subiendo entidades v1

--- SquadSecurity.Shared/Entities/RevisionDet.cs
+++ SquadSecurity.Shared/Entities/RevisionDet.cs
@@ -8,5 +8,17 @@ namespace SquadSecurity.Shared.Entities
 {
     public class RevisionDet
     {
+        public int Codigo { get; set; }
+        public int CodigoCabecera { get; set; }
+        public int CodigoIniciativa { get; set; }
+        public int CodigoHabilitador { get; set; }
+        public int CodigoSquad { get; set; }
+        public string Entregable { get; set; }
+        public int CodigoEstadoCumplimiento { get; set; }
+        public int CodigoExcepcion { get; set; }
+        public int Comentario { get; set; }
+        public string EstadoAuditoria { get; set; }
+        public DateTime FechaAuditoria { get; set; }
+        public string UsuarioAuditoria { get; set; }
     }
 }
